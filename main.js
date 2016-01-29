c096c08c80a3f82289d2d14b427e4bd471b655e2
Josh <jalexy12@gmail.com>
2016-01-28 19:12:00 -0500

    First 3 iterations of the quiz exercise

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ var Question = require('./lib/question.js');
 var Quiz = require('./lib/quiz.js');
 
 var questions = [
-	new Question("What is your name?", "Josh"),
-	new Question("What color is the sky", "Blue"),
-	new Question("What company do you work for?", "Ironhack")
+	new Question("What is your name?", "Josh", 2),
+	new Question("What color is the sky", "Blue", 1),
+	new Question("What company do you work for?", "<I></I>ronhack", 3)
 ]
 
 var quiz = new Quiz(questions);
